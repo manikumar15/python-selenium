ca1af066bc0544c081f5e7971e619557ac8b3859
mani kumar <34789553+manikumar15@users.noreply.github.com>
2019-09-03 16:02:23 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+browser = webdriver.Chrome("C:\\Users\\mani\\Desktop\\selenium\\drivers\\chromedriver.exe")
+browser.get('https://www.google.com/')
+
+search = browser.find_elements_by_css_selector("input[name=q]")
+search[0].send_keys('facebook')
+
+browser.get('https://www.facebook.com/')
+
+username = browser.find_elements_by_css_selector("input[name=email]")
+username[0].send_keys('manikumarvikas')
+
+password = browser.find_elements_by_css_selector("input[name=pass]")
+password[0].send_keys('9573762293')
+
+loginButton = browser.find_elements_by_css_selector("input[type=submit]")
+loginButton[0].click()
+
+time.sleep(3)
+browser.quit()
